f41b4a12f81d2dac43f5e23cdbf87a39a4c9acff
daffaidf <ddfa264@gmail.com>
2025-09-15 17:07:05 +0700

    first commit

---
+++ Daa3.py
@@ -0,0 +1,18 @@
+import datetime 
+
+x = datetime.datetime(2022, 12, 31)
+print(x)
+
+today = datetime.date.today()
+
+mahasiswa = ["Muhammad Daffa Rinaldi", 2024071024, "Informatika" ,"DAA", "Universitas", today]
+print(mahasiswa)
+print("NIM1", mahasiswa[1])
+
+nested_tuple = (100(200,400,600),300(400,800))
+print(nested_tuple)
+
+import pandas as pd
+data = {
+    
+}
